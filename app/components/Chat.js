ae7ae02e3528945927c59b938a7f0c7fea3053ab
Wilkins Fernandez <wilkinsf@gmail.com>
2024-03-08 08:52:08 -0500

    add styles

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -3,10 +3,13 @@ export default function Chat(props) {
 
   return(
     <section>
-      <div>{children}</div>
+      <div
+        className="max-h-30vh relative overflow-y-scroll mb-2"
+      >{children}</div>
       <form onSubmit={onSubmit}>
         {loading && <p>Loading...</p>}
         <input
+          className="w-full border-2 p-2 rounded-xl"
           value={value}
           placeholder={'hi'}
           onChange={onChange}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 'use client';
 
-import { useChat } from 'ai/react';
-import { initialMessagesChat } from "@/app/lib/constants";
+import {useChat} from 'ai/react';
+import {initialMessagesChat} from "@/app/lib/constants";
 import Chat from "@/app/components/Chat";
+
+const styles = {
+  user: "text-right bg-blue-100 ml-auto",
+  assistant: "bg-gray-100 mr-auto",
+  system: "bg-red-100 text-sm rounded-none"
+}
 
 export default function Page() {
   const {
@@ -25,23 +31,39 @@ export default function Page() {
   }
 
   return (
-    <Chat
-    messages={messagesChat}
-    onSubmit={handleSubmit}
-    loading={isLoadingChat}
-    value={inputChat}
-    onChange={handleInputChange}
-    >
-      <div>
-        {messagesChat.map(m => (
-          <div key={m.id}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </div>
-        ))}
+    <main className="w-full lg:w-1/2 m-auto max-w-sm p-4">
+      <Chat
+        messages={messagesChat}
+        onSubmit={handleSubmit}
+        loading={isLoadingChat}
+        value={inputChat}
+        onChange={handleInputChange}
+      >
+        <div>
+          {messagesChat.map((m, index) => {
+            // if (index <= initialMessagesChat.length) return;
+            const {role, id, content, createdAt = new Date()} = m;
+
+            return (
+              <div key={id}>
+                <div className={
+                `${styles[role]} rounded-2xl p-4 my-2 max-w-64`
+                }>
+                  <p>{content}</p>
+                </div>
+                <time
+                  dateTime={createdAt.toString()}
+                  className="text-center text-xs block"
+                >
+                  {new Date(createdAt).toLocaleString()}
+                </time>
+              </div>
+            )
+          })}
 
 
-      </div>
-    </Chat>
+        </div>
+      </Chat>
+    </main>
   );
 }
